6c59981e6c722e8fdf956d986a963301382de816
kellangg <greenekellan50@gmail.com>
2025-03-23 11:30:28 -0500

    Add files via upload

--- slicer.js
+++ slicer.js
@@ -6,8 +6,24 @@ document.getElementById("sliceBtn").addEventListener("click", () => {
     return;
   }
 
-  // Simulated slicing output
-  const gcode = "; Simulated G-code for: " + fileInput.files[0].name + "\nG28 ; home all axes\nG1 Z0.2 F3000 ; move to layer height\nM104 S200 ; set extruder temp\n";
+  const scale = document.getElementById("scale").value;
+  const rx = document.getElementById("rotateX").value;
+  const ry = document.getElementById("rotateY").value;
+  const rz = document.getElementById("rotateZ").value;
+  const infill = document.getElementById("infill").value;
+  const supports = document.getElementById("supports").checked;
+
+  const gcode = [
+    "; G-code for: " + fileInput.files[0].name,
+    "; Scale: " + scale + "%",
+    "; Rotation: X=" + rx + " Y=" + ry + " Z=" + rz,
+    "; Infill: " + infill + "%",
+    "; Supports: " + (supports ? "Enabled" : "Disabled"),
+    "G28 ; home all axes",
+    "G1 Z0.2 F3000 ; move to layer height",
+    "M104 S200 ; set extruder temp"
+  ].join("\n");
+
   const blob = new Blob([gcode], { type: "text/plain" });
   const link = document.createElement("a");
   link.href = URL.createObjectURL(blob);
